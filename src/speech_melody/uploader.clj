cbad4fe24b173598716dbb0cb98e75febf73441c
Paweł Stroiński <pol84@live.com>
2015-05-23 14:38:56 +0100

    Rework uploader as we do not want to upload a track 120 times if soundcloud is processing tracks slowly at the moment

--- src/speech_melody/uploader.clj
+++ src/speech_melody/uploader.clj
@@ -3,9 +3,9 @@
   (:import [com.soundcloud.api ApiWrapper Request Endpoints Params$Track Http]
            [org.apache.http HttpStatus]))
 
-(defmulti #^{:private true} try-upload (fn [provider & _] provider))
+(defmulti upload (fn [provider & _] provider))
 
-(defn- soundcloud-request [wrapper input title author tags description]
+(defn- post-request-to-soundcloud [wrapper input title author tags description]
   (let [request (Request/to Endpoints/TRACKS nil)]
     (doto request
       (.add Params$Track/TITLE title)
@@ -16,29 +16,31 @@
       (.withFile Params$Track/ASSET_DATA input))
     (.post wrapper request)))
 
-(defn- soundcloud-finished-processing [wrapper uri]
-  (let [request (Request/to uri nil)
-        state (-> (.get wrapper request) Http/getJSON (.get "state"))]
-    (when (= state "processing")
-      (println "Processing" uri)
-      (throw (ex-info "Still processing" {:uri uri})))))
-
-(defmethod try-upload :soundcloud [_ input title author config]
-  (let [cfg (:soundcloud config)
-        wrapper (ApiWrapper. (:client-id cfg) (:client-secret cfg) nil nil)
-        _ (.login wrapper (:username cfg) (:password cfg) nil)
-        tags "Blues Classical Electronic Speech"
+(defn- upload-to-soundcloud [cfg wrapper input title author]
+  (.login wrapper (:username cfg) (:password cfg) nil)
+  (let [tags "Blues Classical Electronic Speech"
         description "some description"
-        response (soundcloud-request wrapper input title author tags description)
+        response (post-request-to-soundcloud wrapper input title author tags description)
         status-code (.. response getStatusLine getStatusCode)
         reason (.. response getStatusLine getReasonPhrase)
         json (Http/getJSON response)]
     (when (not= status-code HttpStatus/SC_CREATED)
       (throw (ex-info "Unexpected status code"
                       {:status-code status-code, :reason reason, :args [input title], :json json})))
-    (Thread/sleep 5000)
-    (try-try-again {:sleep 5000 :tries 120} soundcloud-finished-processing wrapper (.get json "uri"))
-    (.get json "permalink_url")))
+    json))
 
-(defn upload [& args]
-  (try-try-again {:sleep 5000 :tries 120} apply try-upload args))
+(defn- ensure-soundcloud-has-finished-processing [wrapper uri]
+  (let [request (Request/to uri nil)
+        state (-> (.get wrapper request) Http/getJSON (.get "state"))]
+    (when (= state "processing")
+      (println "Processing" uri)
+      (throw (ex-info "Still processing" {:uri uri})))))
+
+(defmethod upload :soundcloud [_ input title author config]
+  (let [cfg (:soundcloud config)
+        wrapper (ApiWrapper. (:client-id cfg) (:client-secret cfg) nil nil)
+        try-options {:sleep 1000, :decay :double, :tries 15} ; about 136 mins max and 273 mins total
+        json (try-try-again try-options upload-to-soundcloud cfg wrapper input title author)]
+    (Thread/sleep 5000)
+    (try-try-again try-options ensure-soundcloud-has-finished-processing wrapper (.get json "uri"))
+    (.get json "permalink_url")))
