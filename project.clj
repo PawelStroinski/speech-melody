fac2381fc3f7feb1bfdc2847a1164a1d80b78851
Paweł Stroiński <pol84@live.com>
2015-06-05 22:28:43 +0100

    Add RabbitMQ consumer/publisher, parallelism, logging & error handling, text truncating, refactor make-metadata

--- project.clj
+++ project.clj
@@ -3,13 +3,16 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [overtone "0.9.1"]
                  [clj-http "1.1.1"]
                  [leipzig "0.9.0"]
                  [com.soundcloud/java-api-wrapper "1.3.1"]
                  [robert/bruce "0.7.1"]
                  [org.clojure/data.json "0.2.6"]
+                 [com.novemberain/langohr "3.2.0"]
+                 [log4j/log4j "1.2.17"]
+                 [org.clojure/tools.logging "0.3.1"]
                  ]
   :main ^:skip-aot speech-melody.core
   :target-path "target/%s"
--- src/speech_melody/core.clj
+++ src/speech_melody/core.clj
@@ -1,31 +1,85 @@
 (ns speech-melody.core
   (:require [clojure.java.io :as io]
             [clojure.edn :as edn]
+            [clojure.stacktrace :as strace]
+            [clojure.tools.logging :as log]
             [speech-melody.generator :refer [generate]]
             [speech-melody.uploader :refer [upload]]
             [speech-melody.downloader :refer [download]]
-            [speech-melody.language :refer [text-lang]]))
+            [speech-melody.language :refer [text-lang]]
+            [langohr.core :as lcore]
+            [langohr.channel :as lchannel]
+            [langohr.queue :as lqueue]
+            [langohr.consumers :as lconsumers]
+            [langohr.basic :as lbasic]
+            [langohr.confirm :as lconfirm]))
 
-(defn read-config []
+(defn- read-config []
   (-> "config.edn" io/resource io/file slurp edn/read-string))
 
-(defn make-metadata [{:keys [text user]} {:keys [metadata] :as config}]
-  (let [format-map (fn [m k arg] (assoc m k (format (k m) arg)))
-        textlang (text-lang text config)]
-    (-> metadata
-        (merge textlang)
-        (format-map :title text)
-        (format-map :author user)
-        (format-map :description (:lang textlang)))))
+(defmacro ^{:private true} log-err-for [context & forms]
+  `(try
+     ~@forms
+     (catch Exception error#
+       (log/error
+         (with-out-str
+           (println " [x] Error:" (.getMessage error#) (or (ex-data error#) ""))
+           (println "     Context:" ~context)
+           (print "     Stack trace: ")
+           (strace/print-cause-trace error#))))))
 
-(let [config (read-config)
-      metadata (make-metadata {:text "Hello World", :user "Paweł"} config)
-      temp-mp3 (download metadata config)
-      temp-out (generate temp-mp3 metadata)
-      url (upload temp-out metadata config)]
-  (println "Downloaded to" temp-mp3)
-  (println "Written to" temp-out)
-  (println "Uploaded to" url)
-  (.delete temp-mp3)
-  (.delete temp-out)
-  )
+(defn- truncate [s max-len]
+  (if (> (count s) max-len) (subs s 0 max-len) s))
+
+(defn- make-metadata [{:keys [user] :as msg} {:keys [metadata max-len] :as config}]
+  (let [{:keys [text lang] :as textlang} (-> (text-lang (:text msg) config)
+                                             (update-in [:text] truncate max-len))]
+    (-> (merge msg metadata textlang)
+        (update-in [:title] format text)
+        (update-in [:author] format user)
+        (update-in [:description] format lang))))
+
+(defn- send-done [channel metadata {:keys [done-queue]}]
+  (let [default-exchange-name ""
+        msg (pr-str metadata)]
+    (lbasic/publish channel default-exchange-name done-queue msg {:persistent true})
+    (lconfirm/wait-for-confirms-or-die channel)
+    (log/info " [x] Sent" msg "to" done-queue)))
+
+(def ^{:private true} soundcard (agent nil))
+
+(defn- handle-todo [config channel {:keys [delivery-tag]} ^bytes payload]
+  (log-err-for
+    (seq payload)
+    (let [msg (-> (String. payload "UTF-8") edn/read-string)
+          _ (log/info " [x] Received" msg)
+          metadata (make-metadata msg config)
+          temp-mp3 (download metadata config)
+          _ (log/info " [x] Downloaded to" temp-mp3)
+          f (fn [_]
+              (log-err-for
+                msg
+                (let [temp-out (generate temp-mp3 metadata)]
+                  (future
+                    (log-err-for
+                      msg
+                      (log/info " [x] Written to" temp-out)
+                      (.delete temp-mp3)
+                      (let [url (upload temp-out metadata config)]
+                        (log/info " [x] Uploaded to" url)
+                        (.delete temp-out)
+                        (send-done channel (assoc metadata :url url) config)
+                        (lbasic/ack channel delivery-tag)))))))]
+      (send-off soundcard f))))
+
+(defn -main []
+  (let [config (read-config)
+        connection (lcore/connect)
+        channel (lchannel/open connection)
+        {:keys [todo-queue done-queue]} config
+        todo-handler (fn [& args] (future (apply handle-todo config args)))]
+    (lqueue/declare channel todo-queue {:durable true :auto-delete false})
+    (lqueue/declare channel done-queue {:durable true :auto-delete false})
+    (lconfirm/select channel)
+    (log/info (str " [*] Subscribing to " todo-queue "..."))
+    (lconsumers/blocking-subscribe channel todo-queue todo-handler {:auto-ack false})))
