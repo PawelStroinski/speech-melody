cefbf49dc1c5f0e089131afb45b4fcb1294b0d3a
Paweł Stroiński <pol84@live.com>
2015-06-06 13:52:07 +0100

    A very simple Twitter bot in Ruby - and logging to a file and amend the tweet template

--- ruby_twitter_bot/bot.rb
+++ ruby_twitter_bot/bot.rb
@@ -7,9 +7,16 @@ require 'twitter'
 require 'bunny'
 require 'retryable'
 require 'edn'
+require 'logger'
 
 store = YAML::Store.new('bot.yaml')
 retryable_options = {:tries => 15, :sleep => lambda { |n| 2 ** (n + 1) }}
+$logger = Logger.new('ruby_twitter_bot.log', 10, 1024 * 1024)
+
+def log(*args)
+  $logger.info *args
+  puts *args
+end
 
 client = Twitter::REST::Client.new do |config|
   store.transaction do
@@ -25,14 +32,14 @@ connection.start
 
 done_channel = connection.create_channel
 done_queue = done_channel.queue('speech-melody.done', :durable => true)
-puts " <*> Waiting for messages in #{done_queue.name}"
+log " <*> Waiting for messages in #{done_queue.name}"
 done_queue.subscribe(:manual_ack => true) do |delivery_info, properties, body|
-  puts " <x> Received #{body}"
+  log " <x> Received #{body}"
   msg = EDN.read(body)
   tweet = Retryable.retryable(retryable_options) do
-    client.update("Hey @#{msg[:user]}! Here you go, #{msg[:url]}", :in_reply_to_status_id => msg[:id])
+    client.update("Hey @#{msg[:user]}! There you go: #{msg[:url]}", :in_reply_to_status_id => msg[:id])
   end
-  puts " <x> Tweeted '#{tweet.text}' that has id = #{tweet.id}"
+  log " <x> Tweeted '#{tweet.text}' that has id = #{tweet.id}"
   done_channel.ack(delivery_info.delivery_tag)
 end
 
@@ -41,7 +48,7 @@ todo_queue = todo_channel.queue('speech-melody.todo', :durable => true)
 todo_channel.confirm_select
 handle, since_id = store.transaction { [store[:handle], store.fetch(:since_id, 1)] }
 loop do
-  puts " [*] Fetching Twitter with since_id = #{since_id}"
+  log " [*] Fetching Twitter with since_id = #{since_id}"
   tweets = Retryable.retryable(retryable_options) do
     client.mentions_timeline(:since_id => since_id, :count => 200).sort_by { |tweet| tweet.id }
   end
@@ -51,14 +58,14 @@ loop do
     todo_queue.publish(msg, :persistent => true)
     success = todo_channel.wait_for_confirms
     if success
-      puts " [x] Sent #{msg} to #{todo_queue.name}"
+      log " [x] Sent #{msg} to #{todo_queue.name}"
     else
-      puts " [ ] Message nacked, will retry (#{msg})"
+      log " [ ] Message nacked, will retry (#{msg})"
       break
     end
     since_id = tweet.id
     store.transaction { store[:since_id] = since_id }
-    puts " [*] Set since_id = #{since_id}"
+    log " [*] Set since_id = #{since_id}"
   end
   sleep 65
 end
